5077967e2861fbc08817796d015b01dc51fc741f
Edgar <efqr811@gmail.com>
2022-02-15 22:16:25 -0700

    Started task window setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./styles/style.scss"
+
+class Task {
+  constructor (name, category, breakSetup) {
+    this.name = name;
+    this.category = category;
+    this.breakSetup = breakSetup
+  }
+  focusDuration = 0;
+
+  addTaskHTML() {
+
+  }
+  countUp() {
+
+  }
+  setStartTime() {
+
+  }
+  setStopTime() {
+    
+  }
+  deleteTask() {
+
+  }
+}
+
+const currentTasks = [];
+const taskSetupWindow = () => {
+  
+}
+const addTask = () => {
+  const taskObj = new Task('name', 'coding', '4');
+
+}
